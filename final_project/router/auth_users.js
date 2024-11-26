5083f30b85eb3feea6449a702508a49def5c1cc6
C0smic01 <111181026+C0smic01@users.noreply.github.com>
2024-11-26 23:35:51 +0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,27 +2,88 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+const JWT_SECRET = "secret";
 
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    for (let i = 0; i < users.length; i++)
+    {
+        if (users[i].username === username)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    for (let i = 0; i < users.length; i++)
+    {
+        if (users[i].username === username && users[i].password === password)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try {
+        const {username, password} = req.body;
+        if (!authenticatedUser(username, password))
+        {
+            return res.json({ message: "User is not registered!" });
+        }
+        return res.json({token: jwt.sign({user: username}, JWT_SECRET)})
+    }
+    catch (error)
+    {
+        return res.status(500).json({ message: "Internal Server Error!!" });
+    }
 });
+
+const checkAuthentication = (req, res) => {
+    let token = req.header('Authorization');
+    if (!token) {
+        return res.status(401).json({ message: "No token" });
+    }
+    if (token.startsWith('Bearer ')) {
+        tokenValue = token.slice(7, token.length).trimLeft();
+    }
+
+    try {
+        verificationStatus = jwt.verify(tokenValue, JWT_SECRET);
+        if (!isValid(verificationStatus.username)) {
+            return res.status(401).json({ message: "Please login" });
+        }
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+}
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    // checkAuthentication(req, res);
+    let keys = Object.keys(books);
+    for (let i = 0; i < keys.length; i++)
+    {
+        let id = keys[i];
+        if (books[id].isbn === req.params.isbn)
+        {
+            books[id].reviews = {};
+            return res.status(204).json({message: "Review deleted successfully"});
+        }
+    }
+    return res.json({message: "No reviews found"});
+
 });
 
 module.exports.authenticated = regd_users;
